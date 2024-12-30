76eaff4316a6034ca69d9ff5e7a4bb4fef720903
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-12-30 20:19:31 +0530

    fix gap

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             className="bg-dark min-h-screen flex items-center justify-center"
         >
             <div className='flex flex-col md:flex-row gap-5 items-center justify-center'>
-                <div className='flex flex-row w-full md:w-1/2 gap-12 md:gap-24 mt-32 md:mt-16'>
+                <div className='flex flex-row w-full md:w-1/2 gap-8 md:gap-24 mt-32 md:mt-16'>
                     <div className='flex flex-col gap-6 px-4 items-center'>
                         <h1 className='text-[70px] text-white border-b-2'>01</h1>
                         <div className='h-[100px] md:h-[200px] w-[1px] bg-white'></div>
