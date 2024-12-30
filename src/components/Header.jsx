b57e4a49d9fcaca37bb2698b3239bba77feb7503
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-12-30 19:31:50 +0530

    create menu item in header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import { MdMenu } from 'react-icons/md';
 
 function Header() {
+  const [id, setId] = useState('hero');
+  const [visible, setVisible] = useState(false);
 
-  const [id,setId] = useState('hero');
+  const menuItem = [
+    { name: 'HOME', id: 'hero' },
+    { name: 'ABOUT', id: 'about' },
+    { name: 'PROJECTS', id: 'projects' },
+    { name: 'CONTACT', id: 'contact' },
+  ];
+
+  const changeVisibility = () => {
+    setVisible((prev) => !prev);
+  };
+
   const handleScroll = (id) => {
     const element = document.getElementById(id);
     setId(id);
@@ -11,52 +24,61 @@ function Header() {
     }
   };
 
+  const menu = useMemo(() => {
+    return menuItem.map((item) => (
+      <li className="" key={item.id}>
+        <button
+          onClick={() => handleScroll(item.id)}
+          className="relative flex justify-center text-white font-sans"
+        >
+          <p className="relative z-10">{item.name}</p>
+          {id === item.id && (
+            <div className="absolute top-1/2 transform -translate-y-1/2 bg-accent-purple h-1 w-16 z-0"></div>
+          )}
+        </button>
+      </li>
+    ));
+  }, [id]);
+
   return (
-    <header className="fixed top-0 w-full bg-dark mt-24 z-50">
-      <nav className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <div className="flex flex-wrap text-[32px] md:text-[64px] font-sans text-white">Sachith Abywardhana</div>
-        <ul className="flex space-x-6">
-          <li className=''>
-            <button onClick={() => handleScroll('hero')} className='relative flex justify-center text-white font-sans'>
-              <p className='relative z-10'>HOME</p>
-              {
-                id === 'hero' && (
-                  <div className='absolute  top-1/2 transform -translate-y-1/2  bg-accent-purple h-1 w-16 z-0'></div>
-                )
-              }
-            </button>
-          </li>
-          <li>
-            <button onClick={() => handleScroll('about')} className='relative flex justify-center text-white font-sans'>
-              <p className='relative z-10'>ABOUT</p>
-              {
-                id === 'about' && (
-                  <div className='absolute  top-1/2 transform -translate-y-1/2  bg-accent-purple h-1 w-16 z-0'></div>
-                )
-              }
-            </button>
-          </li>
-          <li>
-            <button onClick={() => handleScroll('projects')} className='relative flex justify-center text-white font-sans'>
-              <p className='relative z-10'>PROJECTS</p>
-              {
-                id === 'projects' && (
-                  <div className='absolute  top-1/2 transform -translate-y-1/2  bg-accent-purple h-1 w-24 z-0'></div>
-                )
-              }
-            </button>
-          </li>
-          <li>
-            <button onClick={() => handleScroll('contact')} className='relative flex justify-center text-white font-sans'>
-              <p className='relative z-10'>CONTACT</p>
-              {
-                id === 'contact' && (
-                  <div className='absolute  top-1/2 transform -translate-y-1/2  bg-accent-purple h-1 w-24 z-0'></div>
-                )
-              }
-            </button>
-          </li>
-        </ul>
+    <header className="fixed top-0 w-full bg-dark mt-16 z-50">
+      <nav className="relative container mx-auto px-4 py-3 flex justify-between items-center">
+        <div className="flex flex-wrap text-[32px] md:text-[64px] font-sans text-white">
+          Sachith Abywardhana
+        </div>
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6">{menu}</ul>
+        
+        {/* Hamburger Menu Icon for Mobile */}
+        <MdMenu
+          className="flex md:hidden text-white cursor-pointer"
+          size={32}
+          onClick={changeVisibility}
+        />
+        
+        {/* Mobile Menu */}
+        {visible && (
+          <div className="absolute top-0 right-3 w-full h-full bg-dark bg-opacity-90 flex justify-end mt-28 items-center z-40">
+            <ul className="flex flex-col items-center space-y-6 text-white">
+              {menuItem.map((item) => (
+                <li key={item.id}>
+                  <button
+                    onClick={() => {
+                      handleScroll(item.id);
+                      setVisible(false); // Close the menu after clicking an item
+                    }}
+                    className="relative flex justify-center text-white font-sans"
+                  >
+                    <p className="relative z-10">{item.name}</p>
+                    {id === item.id && (
+                      <div className="absolute top-1/2 transform -translate-y-1/2 bg-accent-purple h-1 w-16 z-0"></div>
+                    )}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </nav>
     </header>
   );
