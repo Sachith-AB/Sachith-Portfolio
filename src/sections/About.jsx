52f9024d3a0cf7363d1d37b1b2a247d7aa30a86a
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-01-02 11:47:40 +0530

    fix about page

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import about from '../../assets/About.jpg';
 
 function About() {
     return (
         <section
             id="about"
-            className="bg-dark min-h-screen flex items-center justify-center"
+            className="bg-dark min-h-screen "
         >
-            <div className='flex flex-col md:flex-row gap-5 items-center justify-center'>
+            <div className='flex flex-col md:flex-row gap-5  items-center justify-center'>
                 <div className='flex flex-row w-full md:w-1/2 gap-8 md:gap-24 mt-32 md:mt-16'>
                     <div className='flex flex-col gap-6 px-4 items-center'>
                         <h1 className='text-[70px] text-white border-b-2'>02</h1>
@@ -23,9 +24,9 @@ function About() {
                         </div>
                     </div>
 
-                    <div className='flex flex-col flex-wrap justify-start mt-3'>
+                    <div className='flex flex-col flex-1 flex-wrap justify-start mt-3'>
                         <h1 className='text-[32px] md:text-[70px] text-white'>About Me</h1>
-                        <div className='text-white pr-4 flex flex-wrap'>
+                        <div className='text-white pr-4 flex flex-wrap w-full'>
                             <p className='text-[20px] md:text-[24px] text-slate-100'>
                                 I am a passionate Web and Mobile Developer with a 
                                 strong foundation in building dynamic and responsive 
@@ -38,6 +39,9 @@ function About() {
                             </p>
                         </div>
                     </div>
+                </div>
+                <div className="h-80 w-80 md:w-96 md:h-96 rounded-full overflow-hidden flex mt-20">
+                    <img src={about} alt="" />
                 </div>
             </div>
         </section>
